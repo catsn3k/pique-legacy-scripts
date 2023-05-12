e6220414968ac2667d67032e07148140498f6d8b
Rockmaster <rockmasterpwnsnoobs@gmail.com>
2023-05-11 23:55:48 -0600

    Add files via upload

---
+++ airstrike.py
@@ -0,0 +1,137 @@
+"""
+airstrike.py by catsn3k and utf-4096 https://github.com/utf-4096
+Airstrikes. Boom!
+
+Commands
+^^^^^^^^
+
+* ``/airstrike or /a`` allows a player to call an airstike
+* ``/givestrike <player>`` gives a player the ability to call an airstike immediately *admin only*
+"""
+
+from pyspades.contained import OrientationData, GrenadePacket
+from pyspades.common import coordinates, Vertex3
+from pyspades.collision import distance_3d_vector
+from pyspades.world import Grenade
+from twisted.internet import reactor
+from piqueserver.commands import command, admin, get_player, player_only, target_player
+import random
+
+grenade_packet = GrenadePacket()
+
+@command('airstrike', 'a')
+@player_only
+def airstrike(connection, value = None):
+    return connection.start_airstrike(value)
+
+@command('givestrike', admin_only=True)
+@target_player
+def givestrike(connection, player):
+    player.airstrike = True
+    return 'Airstrike support ready! Use with e.g. /airstrike A1'
+
+def apply_script(protocol, connection, config):
+    class AirstrikeConnection(connection):
+        airstrike = False
+        def desync_grenade(self, x, y, z, orientation_x, fuse):
+            """Gives the appearance of a grenade appearing from thin air"""
+            if self.name is None:
+                return
+            grenade_packet.value = fuse
+            grenade_packet.player_id = self.player_id
+            grenade_packet.position = (x, y, z)
+            grenade_packet.velocity = (orientation_x, 0.0, 0.5)
+            self.protocol.send_contained(grenade_packet)
+            position = Vertex3(x, y, z)
+            velocity = Vertex3(orientation_x, 0.0, 0.5)
+            airstrike = self.protocol.world.create_object(Grenade, fuse, 
+                position, None, 
+                velocity, self.grenade_exploded)
+            airstrike.name = 'airstrike'
+            
+        # airstrike
+        
+        def start_airstrike(self, value = None):
+            score_req = self.protocol.airstrike_min_score_req
+            streak_req = self.protocol.airstrike_streak_req
+            interval = self.protocol.airstrike_interval
+            if value is None and (self.god or self.airstrike):
+                return 'Airstrike support ready! Use with e.g. /airstrike A1'
+
+            if not self.airstrike:
+                if self.kills < score_req:
+                    return ('You need a total score of %s (kills or intel) to '
+                        'unlock airstrikes!' % score_req)
+                else:
+                    kills_left = streak_req - (self.streak % streak_req)
+                    return ('Every %s consecutive kills (without dying) you '
+                        'get an airstrike. %s kills to go!' %
+                        (streak_req, kills_left))
+            try:
+                x, y = coordinates(value)
+            except (ValueError):
+                return ("Bad coordinates: should be like 'A4', 'G5'. Look "
+                    "them up in the map.")
+            last_airstrike = self.protocol.last_airstrike[self.team.id]
+            if (last_airstrike and reactor.seconds() - last_airstrike < interval):
+                remain = interval - (reactor.seconds() - last_airstrike)
+                return ('%s seconds before your team can launch '
+                    'another airstrike.' % int(remain))
+            self.protocol.last_airstrike[self.team.id] = reactor.seconds()
+            
+            self.airstrike = False
+            self.protocol.send_chat('Ally %s called in an airstrike on '
+                'location %s' % (self.name, value.upper()), global_message = False,
+                team = self.team)
+            self.protocol.send_chat('[WARNING] Enemy air support heading to %s!' %
+                value.upper(), global_message = False, team = self.team.other)
+            reactor.callLater(2.5, self.do_airstrike, x, y)
+        
+        def do_airstrike(self, start_x, start_y):
+            if self.name is None:
+                return
+            z = 1
+            orientation_x = [1.0, -1.0][self.team.id]
+            start_x = max(0, min(512, start_x + [-64, 64][self.team.id]))
+            range_x = [61, -61][self.team.id]
+            increment_x = [5, -5][self.team.id]
+            for round in range(12):
+                x = start_x + random.randrange(64)
+                y = start_y + random.randrange(64)
+                fuse = self.protocol.map.get_height(x + range_x, y) * 0.033
+                for i in range(5):
+                    x += increment_x
+                    time = round * 0.9 + i * 0.14
+                    reactor.callLater(time, self.desync_grenade, x, y, z,
+                        orientation_x, fuse)
+        
+        def add_score(self, score):
+            connection.add_score(self, score)
+            score_req = self.protocol.airstrike_min_score_req
+            streak_req = self.protocol.airstrike_streak_req
+            score_met = (self.kills >= score_req)
+            streak_met = (self.streak >= streak_req)
+            give_strike = False
+            if not score_met:
+                return
+            if self.kills - score < score_req:
+                self.send_chat('You have unlocked airstrike support!')
+                self.send_chat('Each %s-kill streak will clear you for one '
+                               'airstrike.' % streak_req)
+                if streak_met:
+                    give_strike = True
+            if not streak_met:
+                return
+            if (self.streak % streak_req == 0 or give_strike):
+                self.send_chat('Airstrike support ready! Launch with e.g. '
+                               '/airstrike B4')
+                self.airstrike = True
+                self.refill()
+    
+    class AirstrikeProtocol(protocol):
+        last_airstrike = [None, None]
+        airstrike_min_score_req = 15
+        airstrike_streak_req = 6
+        airstrike_interval = 12
+    
+    return AirstrikeProtocol, AirstrikeConnection
---
+++ melee.py
@@ -0,0 +1,65 @@
+"""
+melee.py by catsn3k
+
+melee.py lets players use the Spade tool as a melee weapon
+
+Setup:
+
+    # The amount of damage dealt by a melee hit (default: 20)
+    melee_damage = 20
+
+This line my already be in the config.toml file so you may not have setup at all
+"""
+
+from pyspades.collision import vector_collision
+from pyspades.constants import SPADE_TOOL
+from twisted.internet.task import LoopingCall
+import random
+
+MELEE_DISTANCE = 2.0
+UPDATE_FPS = 10
+
+def apply_script(protocol, connection, config):
+    melee_damage = config.get('melee_damage', 20)
+    class MeleeProtocol(protocol):
+        def __init__(self, *arg, **kw):
+            protocol.__init__(self, *arg, **kw)
+            self.melee_update = LoopingCall(self.update_melee)
+            self.melee_update.start(1.0 / UPDATE_FPS)
+            
+        def update_melee(self):
+            checked = set()
+            for player1 in self.players.values():
+                if player1 in checked or not player1.hp:
+                    continue
+                checked.add(player1)
+                for player2 in self.players.values():
+                    if player2 in checked or not player2.hp:
+                        continue
+                    fire_1 = (player1.tool == SPADE_TOOL and 
+                        player1.world_object.primary_fire)
+                    fire_2 = (player2.tool == SPADE_TOOL and 
+                        player2.world_object.primary_fire)
+                    if fire_1 and fire_2:
+                        fire_1 = bool(random.randrange(2))
+                        fire_2 = not fire_1
+                    if fire_1:
+                        attack_player = player1
+                        other_player = player2
+                    elif fire_2:
+                        attack_player = player2
+                        other_player = player1
+                    else:
+                        continue
+                    if vector_collision(player1.world_object.position, 
+                                        player2.world_object.position,
+                                        MELEE_DISTANCE):
+                        hit_amount = melee_damage
+                        returned = attack_player.on_hit(hit_amount, other_player, type, None)
+                        if returned == False:
+                            continue
+                        elif returned is not None:
+                            hit_amount = returned
+                        other_player.hit(hit_amount, attack_player)
+                        checked.add(other_player)
+    return MeleeProtocol, connection
